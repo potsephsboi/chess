9c50d2492ae3969ec4e2366a401e82618ac05c06
potsephsboi <adpetrak@gmail.com>
2022-08-13 22:00:48 +0300

    resize imgs

--- assets/pieces/resize.sh
+++ assets/pieces/resize.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-cd black
-
-for img in *
-do
-	convert -resize 101% $img $img
-done
-
 cd ..
-cd white
 
-for img in *
-do
-	convert -resize 101% $img $img
-done
+
+convert -resize 25% position4.png position1.png
+convert -resize 25% position4.png position2.png
+convert -resize 25% position4.png position3.png
+convert -resize 25% position4.png position4.png
+
+
+
+# cd black
+
+# for img in *
+# do
+# 	convert -resize 101% $img $img
+# done
+
+# cd ..
+# cd white
+
+# for img in *
+# do
+# 	convert -resize 101% $img $img
+# done
 
